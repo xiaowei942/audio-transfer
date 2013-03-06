423038873b89c5f15931d6b45cbf6b0d5dd83c6f
FinalReality <finalrealitywei@gmail.com>
2013-03-06 20:48:55 +0800

    Initial commit

---
+++ jni/transfer.c
@@ -0,0 +1,199 @@
+/* Copyright (C) 2008 The Android Open Source Project
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include <linux/ioctl.h>
+
+#include <jni.h>
+
+#include <android/log.h>
+#define LOG_TAG "WEI: "
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)a
+
+#define AUDIO_IOCTL_MAGIC 'a'
+
+#define AUDIO_START        _IOW(AUDIO_IOCTL_MAGIC, 0, unsigned)
+#define AUDIO_STOP         _IOW(AUDIO_IOCTL_MAGIC, 1, unsigned)
+#define AUDIO_FLUSH        _IOW(AUDIO_IOCTL_MAGIC, 2, unsigned)
+#define AUDIO_GET_CONFIG   _IOR(AUDIO_IOCTL_MAGIC, 3, unsigned)
+#define AUDIO_SET_CONFIG   _IOW(AUDIO_IOCTL_MAGIC, 4, unsigned)
+#define AUDIO_GET_STATS    _IOR(AUDIO_IOCTL_MAGIC, 5, unsigned)
+
+/* For big-endin purpose */
+#define ID_RIFF 0x46464952
+#define ID_WAVE 0x45564157
+#define ID_FMT  0x20746d66
+#define ID_DATA 0x61746164
+
+#define FORMAT_PCM 1
+
+struct wav_header {
+	uint32_t riff_id;
+	uint32_t riff_sz;
+	uint32_t riff_fmt;
+	uint32_t fmt_id;
+	uint32_t fmt_sz;
+	uint16_t audio_format;
+	uint16_t num_channels;
+	uint32_t sample_rate;
+	uint32_t byte_rate;       /* sample_rate * num_channels * bps / 8 */
+	uint16_t block_align;     /* num_channels * bps / 8 */
+	uint16_t bits_per_sample;
+	uint32_t data_id;
+	uint32_t data_sz;
+};
+
+struct msm_audio_config {
+    uint32_t buffer_size;
+    uint32_t buffer_count;
+    uint32_t channel_count;
+    uint32_t sample_rate;
+    uint32_t codec_type;
+    uint32_t unused[3];
+};
+
+struct msm_audio_stats {
+    uint32_t out_bytes;
+    uint32_t unused[3];
+};
+
+#define INPUT_FLAG (1<<0) 
+#define OUTPUT_FLAG (1<<1)
+
+struct audio_struct {
+	int fd_input;
+	int fd_output;
+	
+	struct msm_audio_config input_config;
+	struct msm_audio_config output_config;
+	struct msm_audio_stats audio_stats;
+} my_audio_struct;
+
+int open_files(unsigned flags)
+{
+	LOGI("open_file --> flags: %d",flags);
+	if(flags & INPUT_FLAG)
+	{
+		int fd = open("/dev/msm_pcm_in",O_RDWR);
+		if(fd < 0)
+		{
+			LOGE("Cannot open input file");
+			return -1;		
+		}
+		LOGI("Open input success");
+		my_audio_struct.fd_input = fd;
+	}
+
+	if(flags & OUTPUT_FLAG)
+	{
+		int fd = open("/dev/msm_pcm_out",O_RDWR); 
+		if(fd < 0)
+		{
+			LOGE("Cannot open output file");
+			return -1;		
+		}
+		LOGI("Open output success");
+		my_audio_struct.fd_output = fd;
+	}
+	return 0;
+}
+
+int set_params(unsigned play_rate, unsigned play_channels, unsigned rec_rate, unsigned rec_channels, unsigned flags)
+{
+	if(flags & INPUT_FLAG)
+	{
+				
+	}
+
+	if(flags & OUTPUT_FLAG)
+	{
+		if(ioctl(my_audio_struct.fd_output, AUDIO_GET_CONFIG, &my_audio_struct.output_config))
+		{
+			LOGE("Cannot get output config");
+			return -1;
+		}
+
+		my_audio_struct.output_config.channel_count = play_channels;
+		my_audio_struct.output_config.sample_rate = play_rate;
+		
+		LOGI("play_channels: %d",play_channels);
+		LOGI("play_rate: %d",rec_rate);
+
+		if(ioctl(my_audio_struct.fd_output, AUDIO_SET_CONFIG, &my_audio_struct.output_config))
+		{
+			LOGE("Cannot set output config");
+			return -1;
+		}
+	}
+	LOGI("Set params success\n");
+	return 0;
+}
+
+int unit_init(unsigned play_rate, unsigned play_channels, unsigned rec_rate, unsigned rec_channels, unsigned flags)
+{
+	int ret = open_files(flags);
+	if(ret != 0)
+	{
+		return -1;
+	}
+
+	ret = set_params(play_rate, play_channels, rec_rate, rec_channels, flags);
+	if(ret != 0)
+	{
+		return -1;
+	}
+	LOGI("Init success\n");
+	return 1;
+}
+
+int do_play()
+{
+	char *buf;
+	int fd = open("/data/out.wav",O_RDONLY);
+	if(fd<0)
+	{
+		LOGE("Cannot open out.wav");
+	}
+	
+	LOGI("Open out.wav success");
+	
+	buf = malloc(4800);
+
+	lseek(fd, 44, SEEK_SET);
+	for(;;)
+	{
+		LOGI("Now playing");
+		int ret = read(fd, buf,4800);
+		if(ret != 4800)
+		{
+			LOGE("Read data faild");
+		}
+
+		write(my_audio_struct.fd_output, buf, 4800);
+	}
+}
+
+jint Java_com_thinpad_audiotransfer_AudiotransferActivity_unitInit(JNIEnv *env, jobject thiz, jint play_rate, jint play_channels, jint rec_rate, jint rec_channels, jint flags)
+{
+ 	return unit_init(play_rate, play_channels, rec_rate, rec_channels, flags);
+}
+
+jint Java_com_thinpad_audiotransfer_AudiotransferActivity_doPlay()
+{
+	do_play();
+}
+
+#if 0 //For test only
+int main()
+{
+	unit_init(44100,2,44100,2,3);
+}
+#endif
---
+++ src/com/thinpad/audiotransfer/AudiotransferActivity.java
@@ -0,0 +1,55 @@
+package com.thinpad.audiotransfer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.util.Log;
+
+public class AudiotransferActivity extends Activity {
+	/** Called when the activity is first created. */
+	public static final int INPUT_FLAG = 1;
+	public static final int OUTPUT_FLAG = 2;
+
+	private Button btn_init;
+	private Button btn_play;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		btn_init = (Button) findViewById(R.id.btn_init);
+		btn_play = (Button) findViewById(R.id.btn_play);
+		btn_init.setOnClickListener(listener);
+		btn_play.setOnClickListener(listener);
+	}
+
+	public Button.OnClickListener listener = new OnClickListener() {
+
+		@Override
+		public void onClick(View arg0) {
+			// TODO Auto-generated method stub
+			switch (arg0.getId()) {
+			case R.id.btn_init:
+				unitInit(44100, 2, 44100, 2, (OUTPUT_FLAG | INPUT_FLAG));
+				break;
+			case R.id.btn_play:
+				doPlay();
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+
+	public static native int unitInit(int play_rate, int play_channels,
+			int rec_rate, int rec_channels, int flags);
+
+	public static native int doPlay();
+
+	static {
+		System.loadLibrary("transfer");
+	}
+}
